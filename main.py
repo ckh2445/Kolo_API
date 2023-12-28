9153ae6bea5c6f2391e527b1b8bf3c2d8236fe93
pyyolo <kwangho.choi@navercorp.com>
2023-12-28 19:31:06 +0900

    PAYTS-1 GET API - 전체 조회
    PAYTS-1 GET API - 전체 조회

---
+++ main.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI 
+
+app = FastAPI()
+
+@app.get("/")
+def health_check_handler():
+    return {"ping" : "pong"}
+
+todo_data = {
+    1: {
+        "id":1,
+        "contents":"실전! FastAPI 섹션 0 수강",
+        "is_done": True,
+        
+    },
+    2: {
+        "id":2,
+        "contents":"실전! FastAPI 섹션 2 수강",
+        "is_done": False,
+        
+    },
+    3: {
+        "id":3,
+        "contents":"실전! FastAPI 섹션 3 수강",
+        "is_done": False,
+        
+    },
+    
+}
+
+# @app.get("/todos")
+# def get_todos_handler():
+#     return list(todo_data.values())
+
+@app.get("/todos")
+def get_todos_handler(order:str | None = None):
+    ret =  list(todo_data.values())
+    
+    if order and order == "DESC":
+        return ret[::-1]
+    
+    return ret
