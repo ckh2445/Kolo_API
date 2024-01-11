0bc6ab7d31b0b2c4c077d6e6f7216255056306c3
pyyolo <79975403+ckh2445@users.noreply.github.com>
2024-01-11 22:41:50 +0900

    로그인 API 구현

--- src/schema/response.py
+++ src/schema/response.py
@@ -24,3 +24,7 @@ class UserSchema(BaseModel):
 
     class Config:
         orm_mode = True
+
+
+class JWTResponse(BaseModel):
+    access_token: str
--- src/service/user.py
+++ src/service/user.py
@@ -1,8 +1,13 @@
+from datetime import datetime, timedelta
+
 import bcrypt
+from jose import jwt
 
 
 class UserService:
     encoding: str = "UTF-8"
+    secret_key: str = "6b0698748fdd0d0ed38fcc6c239c06115c2dafcd859a6374055cb04f7ab073a6"  # openssl rand -hex 32 data
+    jwt_algorithm: str = "HS256"
 
     def hash_password(self, plain_password: str) -> str:  # 해쉬값은 byte
         hashed_password: bytes = bcrypt.hashpw(
@@ -11,3 +16,25 @@ class UserService:
         )
 
         return hashed_password.decode(self.encoding)
+
+    # 로그인검증
+    def verify_password(
+            self, plain_password: str, hashed_password: str
+    ) -> bool:
+        # try/except 문의 예외 처리가능
+
+        return bcrypt.checkpw(
+            plain_password.encode(self.encoding),
+            hashed_password.encode(self.encoding)
+        )
+
+    # jwt 생성
+    def create_jwt(self, username: dict) -> str:
+        return jwt.encode(
+            {
+                "sub": username, # unique 식별자
+                "exp": datetime.now() + timedelta(days=1)
+            },
+            self.secret_key,
+            algorithm=self.jwt_algorithm,
+        )
